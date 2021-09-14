92ec54fc4c91363776dbfea54c65b4e7df4345b7
oleg.yakubyshyn <oleg.yakubyshyn@ideus.biz>
2021-09-14 17:42:19 +0300

    implementation PayPal IOS SDK

--- src/paypal-checkout.ios.ts
+++ src/paypal-checkout.ios.ts
@@ -8,25 +8,25 @@ export class PaypalCheckout extends Common {
     }
     paypalRequest(options: PaypalOptions): Promise<any> {
         return new Promise(function (resolve, reject) {
-            var context = this;
-
-            var braintreeClient = BTAPIClient.alloc().initWithAuthorization(options.token);
-            var payPalDriver = BTPayPalDriver.alloc().initWithAPIClient(braintreeClient);
-
-            payPalDriver.viewControllerPresentingDelegate = context;
-            payPalDriver.appSwitchDelegate = context; 
-            var request= BTPayPalRequest.alloc().initWithAmount(options.amount);
-            request.currencyCode = options.currencyCode; 
-
-            payPalDriver.requestOneTimePaymentCompletion(request, function(tokenizedPayPalAccount, error) {
-                if (tokenizedPayPalAccount) {
-                    resolve(tokenizedPayPalAccount.nonce);
-                } else if (error) {
-                    reject(error);
-                } else {
-                    reject("Buyer canceled payment approval");
-                }
-            });
+            // var context = this;
+            //
+            // var braintreeClient = BTAPIClient.alloc().initWithAuthorization(options.token);
+            // var payPalDriver = BTPayPalDriver.alloc().initWithAPIClient(braintreeClient);
+            //
+            // payPalDriver.viewControllerPresentingDelegate = context;
+            // payPalDriver.appSwitchDelegate = context;
+            // var request= BTPayPalRequest.alloc().initWithAmount(options.amount);
+            // request.currencyCode = options.currencyCode;
+            //
+            // payPalDriver.requestOneTimePaymentCompletion(request, function(tokenizedPayPalAccount, error) {
+            //     if (tokenizedPayPalAccount) {
+            //         resolve(tokenizedPayPalAccount.nonce);
+            //     } else if (error) {
+            //         reject(error);
+            //     } else {
+            //         reject("Buyer canceled payment approval");
+            //     }
+            // });
         });
     }
 }
